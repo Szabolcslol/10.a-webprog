cdfcdc1461e565f32da06a5524cad4bfa9f40249
Szabolcslol <szilagyiszabee23@gmail.com>
2025-01-30 10:36:56 +0100

    gyakpython1

--- programozas/gyakorlas1/feladat.py
+++ programozas/gyakorlas1/feladat.py
@@ -1,2 +1,12 @@
-
-print('Szia\Hello')
+nev ="Szabolcs"
+nev2 = "Levente"
+szam = 17
+nap = "Hétfő"
+nap2 = "Kedd"
+nap3 = "Szerda"
+nap4 = "Csütörtök"
+nap5 = "Péntek"
+nap6 = "Szombat"
+nap7 = "Vasárnap"
+print(f"A neved: {nev}, másik név={nev2}, ennnyi éves: {szam}")
+print(f"A hét napjai: {nap}\n{nap2}\n{nap3}\n{nap4}\n{nap5}\n{nap6}\n{nap7}")
