7d06ad274c3e683d21fe1a8035a1f62ac05adb80
Szabolcslol <szilagyiszabee23@gmail.com>
2025-02-13 10:34:30 +0100

    gyakpython

---
+++ programozas/gyakorlas2/feladat.py
@@ -0,0 +1,28 @@
+# Szöveg bekérés
+# nev = input("Add meg a neved: ")
+# print("Szia", nev)
+
+
+# Szám bekérés típuskonverzióval
+# kor = int(input("Hány éves vagy? "))
+# print("Jövőre ennyi idős leszel", kor + 1)
+
+
+# Két szám bekérése és műveletvégzés
+# szam1 = int(input("Add meg az első számot: "))
+# szam2 = int(input("Add meg a második számot: "))
+# osszeg = szam1 + szam2
+# print("A két szám összege:", osszeg)
+
+
+#1. feladat
+# szam = int(input("Add meg egy számot: "))
+# negyzet = szam ** 2
+# print("A szám négyzete: ", negyzet)
+
+
+#2. feladat
+szam1 = int(input("Add meg az első számot: "))
+szam2 = int(input("Add meg a második számot: "))
+osszeg = szam1 + szam2
+print("A két szám összege: ", osszeg)
