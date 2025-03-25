2b74c927fdcf1d0c31656bab4d043c48cea39c12
Szabolcslol <szilagyiszabee23@gmail.com>
2025-03-25 09:44:32 +0100

    asd

---
+++ webprog/UniServerZ/www/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <title>Első php programom</title>
+</head>
+<body class="bg-light">
+<header>
+     <p>Ez itt egy fejléc</p>
+  </header>
+  <main class="container mt-5" style="max-width: 400px">
+        <h1 class="text-center mb-4">Összeadás</h1>
+        <form method="POST">
+            <div>
+                <label for="num1" class="form-label">Első szám</label>
+                <input type="number" class="form-control" name="num1" id="num1" required>
+            </div>
+        </form>
+     </main>
+  <footer>
+     <p>Ez itt egy lábléc</p> 
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
