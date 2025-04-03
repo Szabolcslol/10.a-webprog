eb33c129d60bf3ac542373bf58accc96f04f992c
Szabolcslol <szilagyiszabee23@gmail.com>
2025-04-03 10:34:38 +0200

    gyakpython

---
+++ programozas/gyak9/feladat.py
@@ -0,0 +1,62 @@
+# # Lista létrehozása elemekkel
+# gyumolcsok = ["alma", "banán", "cseresznye"]
+
+# # Elem elérése index segítségével
+# print(gyumolcsok[1])  # banán
+
+# # Utolsó elem elérése index segítségével
+# print(gyumolcsok[-1])  # cseresznye
+
+# # Elem módosítása
+# gyumolcsok[1] = "narancs"
+# print(gyumolcsok)  # ['alma', 'narancs', 'cseresznye']
+
+# # Elem törlése
+# del gyumolcsok[0]
+# print(gyumolcsok)  # ['narancs', 'cseresznye']
+
+# # Lista hossza
+# print(len(gyumolcsok))  # 2
+
+# ---------------------------------------------------
+
+# szamok = [3, 1, 4, 1, 5, 9]
+
+# # Elem hozzáadása
+# szamok.append(2)
+# print(szamok)
+
+# # Elem beszúrása
+# szamok.insert(1, 8)
+# print(szamok)
+
+# # Elem törlése
+# szamok.remove(1)
+# print(szamok)
+
+# # Lista rendezése
+# szamok.sort()
+# print(szamok)
+
+# # Lista megfordítása
+# szamok.reverse()
+# print(szamok)
+
+# # Rajta van az 5-ös szám a listán?
+# if 5 in szamok:
+#     print("Az 5 rajta van")
+
+#1. feladat
+filmek = ['avatar', 'star wars', 'harry potter']
+
+filmek.append('terminator')
+print(filmek)
+
+#2. feladat
+filmek.insert(1, 'back to the future')
+print(filmek)
+
+#3. feladat
+filmek.sort()
+print(filmek)
+
