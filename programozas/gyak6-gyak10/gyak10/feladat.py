a8b46324c9a42f448bb8aa018bf968421db5283c
Szabolcslol <szilagyiszabee23@gmail.com>
2025-04-04 09:36:04 +0200

    gyakpython

---
+++ programozas/gyak6-gyak10/gyak10/feladat.py
@@ -0,0 +1,34 @@
+# TUPLE
+
+# gyumolcsok = ["alma", "banán", "cseresznye"] # Lista
+# gyumolcsok = ("alma", "banán", "cseresznye")  # Tuple
+
+# print(gyumolcsok)
+# print(gyumolcsok[1]) # banán
+
+# print(len(gyumolcsok)) # 3
+
+#1. feladat
+# szinek = ("fekete", "kék", "piros", "szürke")
+# print(szinek)
+# print(szinek[1])
+
+#2. feladat
+# szamok = (5, 10, 15, 20)
+# print(szamok)
+# print(len(szamok))
+
+#3. feladat
+# szamok = (5, 10, 15, 20)
+# print(szamok)
+# print(len(szamok))
+# szamok[1] = 7
+
+#4. feladat
+# szamok = (5, 10, 15, 20)
+# print(szamok)
+# print(len(szamok))
+# del szamok[0]
+
+#5. feladat
+szamok = (1, 2, 3, 4, 5,)
