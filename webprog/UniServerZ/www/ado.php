4a320406cd7b90c83712ee9e7ca95d4f7d3515a9
Szabolcslol <szilagyiszabee23@gmail.com>
2025-04-09 13:37:26 +0200

    asd

--- webprog/UniServerZ/www/ado.php
+++ webprog/UniServerZ/www/ado.php
@@ -6,11 +6,11 @@
     <!-- Bootstrap 5.3 CDN betöltése -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <title>Adó számító</title>
+    <title>Adó kalkulátor</title>
     </head>
   <body class="bg-dark text-white d-flex justify-content-center align-items-center vh-100">
     <header>
 
     </header>
     <div class="card bg-black text-white shadow-lg p-4 border-0" style="max-width: 400px; width: 100%">
-            <h1 class="text-center mb-3">Adó számító</h1>
+            <h1 class="text-center mb-3">Adó kalkulátor</h1>
