4b5a600cb79d53486179d607be8413bd07952a3f
Szabolcslol <szilagyiszabee23@gmail.com>
2025-04-10 08:45:37 +0200

    datum php

--- webprog/UniServerZ/www/dobokocka.php
+++ webprog/UniServerZ/www/dobokocka.php
@@ -6,7 +6,7 @@
     <!-- Bootstrap 5.3 CDN betöltése -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <title>Dobókocka kalkulátor</title>
+    <title>Dobókocka</title>
   </head>
   <body class="bg-dark text-white d-flex justify-content-center align-items-center vh-100">
     <main>
