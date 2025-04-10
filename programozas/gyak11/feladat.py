17cd817249d08364708eff33590f75362e6087a8
Szabolcslol <szilagyiszabee23@gmail.com>
2025-04-10 10:26:13 +0200

    pythongyak

---
+++ programozas/gyak11/feladat.py
@@ -0,0 +1,42 @@
+# # Létrehozás
+# szotar = {"alma": "piros", "banán": "sárga"}
+# print(szotar)
+
+# # Elem értékének a lekérdezés
+# print(szotar["alma"])
+
+# # Új kulcs-érték pár hozzáadása
+# szotar["cseresznye"] = "bordó"
+# print(szotar)
+
+# # Érték módosítás
+# szotar["cseresznye"] = "bordó2"
+# print(szotar)
+
+# print(len(szotar))
+
+#1. feladat
+# szotar = {"Az": 1138, "Egri csillagok": 632}
+# print(szotar)
+
+# # #2. feladat
+# print(len(szotar))
+
+# # #3. feladat
+# szotar["Rómeó és Júlia"] = 120
+# print(szotar)
+
+# # #4. feladat
+# print(len(szotar))
+
+# # #5. feladat
+# print(szotar["Az"])
+
+# # #6. feladat
+# # print(szotar["alma"])
+
+# # #7. feladat
+# szotar["Az"] = 1100
+
+# # #8. feladat
+# print(szotar)
