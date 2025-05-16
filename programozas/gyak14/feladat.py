0db47c5c9d863eb517d68af55089134ce1a187f0
Szabolcslol <szilagyiszabee23@gmail.com>
2025-05-16 13:18:06 +0200

    gyakpython

---
+++ programozas/gyak14/feladat.py
@@ -0,0 +1,58 @@
+# gyumolcsok = {"alma", "körte", "szilva"}
+
+# gyumolcsok.add("barack")
+# print(gyumolcsok)
+
+# gyumolcsok.add("barack")
+# print(gyumolcsok)
+
+# gyumolcsok.remove("barack")
+# print(gyumolcsok)
+
+# szamok_lista = [1, 2, 2, 4]
+# print(szamok_lista)
+# halmaz = set(szamok_lista)
+# print(halmaz)
+
+#-------------------------
+
+# halmaz1 = {"alma", "banán", "cseresznye"}
+# halmaz2 = {"alma", "körte", "barack"}
+
+# halmaz3 = halmaz1.union(halmaz2)
+# print(halmaz3)
+
+# halmaz3 = halmaz1 | halmaz2
+# print(halmaz3)
+
+# halmaz3 = halmaz1.difference(halmaz2)
+# print(halmaz3)
+
+# halmaz3 = halmaz1 - halmaz2
+# print(halmaz3)
+
+# halmaz3 = halmaz1.intersection(halmaz2)
+# print(halmaz3)
+
+# halmaz3 = halmaz1 & halmaz2
+# print(halmaz3)
+
+
+#1. feladat
+# halmaz1 = {"kosárlabda", "foci", "röplabda"}
+# print(halmaz1)
+
+# # #2. feladat
+# halmaz1.add("úszás")
+# print(halmaz1)
+
+# # #3. feladat
+# halmaz1.add("úszás")
+# print(halmaz1)
+
+# # #4. feladat
+# halmaz2 = {"kézilabda", "sakk", "lovaglás"}
+# print(halmaz2)
+# halmaz3 = halmaz1.union(halmaz2)
+# print(halmaz3)
+
