a289401d148d8191a4fc5bf00fce30f688b4d36a
Szabolcslol <szilagyiszabee23@gmail.com>
2025-05-30 13:14:41 +0200

    asd

---
+++ programozas/gyak11-gyak15/gyak15/feladat.py
@@ -0,0 +1,4 @@
+termekek = {"alma": 500, "banán": 1000, "eper": 1500, "körte": 2000, "málna": 2500, "cseresznye": 3000, "narancs": 3500, "mandarin": 4000, "barack": 4500, "szilva": 5000}
+vasarlas = {}
+vasarlas = input("Mit szeretnél vásárolni? ")
+darab = input("Hány darabot? ")
